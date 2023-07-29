d3b335620a1b5671346ceb53fe6250372bca3026
Gf0rC3-2 <140288432+Gf0rC3-2@users.noreply.github.com>
2023-07-29 17:37:51 +1000

    Create Kevin's Script: add & delete users.sh
    User Guide:
    Create users.txt:
    Use either “cut -d: -f1 /etc/passwd” or “getent passwd | cut -d: -f1” to list all users.
    The users will be listed with system users at the top and normal users at the bottom. Copy from user ubuntu down.
    Paste into users.txt
    Create allowed.txt
    Copy all allowed users from readme and paste Into allowed.txt
    Run script

---
+++ Kevin's Script: add & delete users.sh
@@ -0,0 +1,12 @@
+!/bin/bash
+allowed_file="allowed.txt"
+users_file="users.txt"
+
+# Compare the users between the two files
+users_to_delete=$(comm -13 <(sort "$allowed_file") <(sort "$users_file"))
+users_to_add=$(comm -13 <(sort "$users_file") <(sort "$allowed_file"))
+
+# Ask for confirmation before deleting users
+read -p "Are you sure you want to delete the following users: $users_to_delete? (y/n) " confirm_delete
+if [[ $confirm_delete == "y" ]]; then
+    vim
